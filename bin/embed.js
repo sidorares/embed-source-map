347b642a75a96acec3585c56fa9c23217525c785
Markus Wolf <markus.wolf@sinnerschrader.com>
2016-06-17 16:27:26 +0200

    Update to latest sourcemap definition
    * Allow //@sour... and //#source...
    * Allow relative pathes to the input file

--- bin/embed.js
+++ bin/embed.js
@@ -3,14 +3,22 @@
 var fs    = require('fs');
 var path  = require('path');
 var embed = require('../index.js');
+var globby = require('globby');
 
 function makeSmInline(name) {
   var src = fs.readFileSync(name, 'utf8');
-  var srcEmbed = embed(src);
+  var srcEmbed = embed(name, src);
   if(src != srcEmbed) {
     fs.writeFileSync(name + '.bak', src);
     fs.writeFileSync(name, srcEmbed);
   }
 }
 
-process.argv.slice(2).forEach(makeSmInline);
+process.argv.slice(2).forEach((arg) => {
+    if (arg.indexOf('*') > -1) {
+        globby(arg)
+            .then(paths => paths.forEach(makeSmInline));
+    } else {
+        makeSmInline(arg);
+    }
+});
--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var convert = require('convert-source-map');
 var fs = require('fs');
 var path = require('path');
 
-function addSources(map) {
+function addSources(basedir, map) {
   if (Array.isArray(map.sourcesContent))
     return;
   map.sourcesContent = [];
   map.sources.forEach(function(name, idx) {
-    map.sourcesContent[idx] = fs.readFileSync(path.join(map.sourceRoot, name), 'utf8');
+    map.sourcesContent[idx] = fs.readFileSync(path.join(basedir, map.sourceRoot, name), 'utf8');
   });
 }
 
@@ -18,10 +18,10 @@ function replaceMatchWith(match, newContent)
   return src.slice(0, match.index) + newContent + src.slice(match.index + match[0].length);
 }
 
-module.exports = function embedSourcemap(src)
+module.exports = function embedSourcemap(name, src)
 {
-  var smMimeEncodedRx = /^[ \t]*\/\/@[ \t]+sourceMappingURL=data:(?:application|text)\/json;base64,(.+)/m;
-  var smCommentRx     = /^[ \t]*\/\/@[ \t]+sourceMappingURL=(.*)/m;
+  var smMimeEncodedRx = /^[ \t]*\/\/[@|#][ \t]+sourceMappingURL=data:(?:application|text)\/json;base64,(.+)/m;
+  var smCommentRx     = /^[ \t]*\/\/[@|#][ \t]+sourceMappingURL=(.*)/m;
   var comment, map;
 
   // already base64 mime encoded.
@@ -30,7 +30,7 @@ module.exports = function embedSourcemap(src)
   if (match)
   {
     map = convert.fromComment(match[0]).sourcemap;
-    addSources(map);
+    addSources(path.dirname(name), map);
     comment = convert.fromObject(map).toComment();
     return replaceMatchWith(match, comment);
   }
@@ -38,8 +38,8 @@ module.exports = function embedSourcemap(src)
   // ref to a file. load, embed sources and convert to base64 mime encoded comment
   match = src.match(smCommentRx);
   if(match) {
-    map = JSON.parse(fs.readFileSync(match[1], 'utf8'));
-    addSources(map);
+    map = JSON.parse(fs.readFileSync(path.resolve(path.dirname(name), match[1]), 'utf8'));
+    addSources(path.dirname(name), map);
     comment = convert.fromObject(map).toComment();
     return replaceMatchWith(match, comment);
   }
